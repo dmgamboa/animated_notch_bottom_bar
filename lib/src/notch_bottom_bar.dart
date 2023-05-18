694d0a5b3723bfd77defdcea138b303ea1b34edd
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2023-05-18 15:27:26 -0700

    Add width param

--- lib/src/notch_bottom_bar.dart
+++ lib/src/notch_bottom_bar.dart
@@ -46,21 +46,24 @@ class AnimatedNotchBottomBar extends StatefulWidget {
   /// Color of bottom bar
   final Color notchColor;
 
-  const AnimatedNotchBottomBar(
-      {Key? key,
-      required this.pageController,
-      required this.bottomBarItems,
-      required this.onTap,
-      this.color = Colors.white,
-      this.itemLabelStyle,
-      this.showShadow = true,
-      this.showLabel = true,
-      this.showBlurBottomBar = false,
-      this.blurOpacity = 0.5,
-      this.blurFilterX = 5.0,
-      this.blurFilterY = 10.0,
-      this.notchColor = Colors.white})
-      : super(key: key);
+  double? width;
+
+  AnimatedNotchBottomBar({
+    Key? key,
+    required this.pageController,
+    required this.bottomBarItems,
+    required this.onTap,
+    this.color = Colors.white,
+    this.itemLabelStyle,
+    this.showShadow = true,
+    this.showLabel = true,
+    this.showBlurBottomBar = false,
+    this.blurOpacity = 0.5,
+    this.blurFilterX = 5.0,
+    this.blurFilterY = 10.0,
+    this.notchColor = Colors.white,
+    this.width,
+  }) : super(key: key);
 
   @override
   _AnimatedNotchBottomBarState createState() => _AnimatedNotchBottomBarState();
@@ -73,7 +76,7 @@ class _AnimatedNotchBottomBarState extends State<AnimatedNotchBottomBar> {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    _screenWidth = MediaQuery.of(context).size.width;
+    _screenWidth = widget.width ?? MediaQuery.of(context).size.width;
   }
 
   @override
@@ -88,97 +91,106 @@ class _AnimatedNotchBottomBarState extends State<AnimatedNotchBottomBar> {
     }
 
     final size = MediaQuery.of(context).size;
-    final width = size.width;
+    final width = widget.width ?? size.width;
     const height = kHeight + kMargin * 2;
 
     return widget.bottomBarItems.length > maxCount
         ? Container()
-        : AnimatedBuilder(
-            animation: widget.pageController,
-            builder: (BuildContext _, Widget? child) {
-              ///to set any initial page
-              double scrollPosition =
-                  widget.pageController.initialPage.toDouble();
-              int currentIndex = widget.pageController.initialPage;
+        : Center(
+            child: AnimatedBuilder(
+              animation: widget.pageController,
+              builder: (BuildContext _, Widget? child) {
+                ///to set any initial page
+                double scrollPosition =
+                    widget.pageController.initialPage.toDouble();
+                int currentIndex = widget.pageController.initialPage;
 
-              if (widget.pageController.hasClients) {
-                scrollPosition = widget.pageController.page!;
-                currentIndex = (widget.pageController.page! + 0.5).toInt();
-              }
+                if (widget.pageController.hasClients) {
+                  scrollPosition = widget.pageController.page!;
+                  currentIndex = (widget.pageController.page! + 0.5).toInt();
+                }
 
-              return ClipRRect(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: Stack(
-                    clipBehavior: Clip.none,
-                    children: <Widget>[
-                      BackdropFilter(
-                        filter: ImageFilter.blur(
-                          sigmaX: widget.showBlurBottomBar
-                              ? widget.blurFilterX
-                              : 0.0,
-                          sigmaY: widget.showBlurBottomBar
-                              ? widget.blurFilterY
-                              : 0.0,
-                        ),
-                        child: Opacity(
-                          opacity:
-                              widget.showBlurBottomBar ? widget.blurOpacity : 1,
-                          child: CustomPaint(
-                            size: Size(width, height),
-                            painter: BottomBarPainter(
-                                position:
-                                    _itemPosByScrollPosition(scrollPosition),
-                                color: widget.color,
-                                showShadow: widget.showShadow,
-                                notchColor: widget.notchColor),
-                          ),
-                        ),
-                      ),
-                      for (var i = 0;
-                          i < widget.bottomBarItems.length;
-                          i++) ...[
-                        if (i == currentIndex)
-                          Positioned(
-                            top: kTopMargin,
-                            left: kCircleRadius -
-                                kCircleMargin / 2 +
-                                _itemPosByScrollPosition(scrollPosition),
-                            child: BottomBarActiveItem(
-                              i,
-                              itemWidget: widget.bottomBarItems[i].activeItem,
-                              scrollPosition: scrollPosition,
-                              onTap: widget.onTap,
+                return Align(
+                  alignment: Alignment.bottomCenter,
+                  child: ClipRRect(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Stack(
+                        clipBehavior: Clip.none,
+                        children: <Widget>[
+                          BackdropFilter(
+                            filter: ImageFilter.blur(
+                              sigmaX: widget.showBlurBottomBar
+                                  ? widget.blurFilterX
+                                  : 0.0,
+                              sigmaY: widget.showBlurBottomBar
+                                  ? widget.blurFilterY
+                                  : 0.0,
+                            ),
+                            child: Opacity(
+                              opacity: widget.showBlurBottomBar
+                                  ? widget.blurOpacity
+                                  : 1,
+                              child: CustomPaint(
+                                size: Size(width, height),
+                                painter: BottomBarPainter(
+                                  position:
+                                      _itemPosByScrollPosition(scrollPosition),
+                                  color: widget.color,
+                                  showShadow: widget.showShadow,
+                                  notchColor: widget.notchColor,
+                                ),
+                              ),
                             ),
                           ),
-                        if (i != currentIndex)
-                          Positioned(
-                            top: kMargin + (kHeight - kCircleRadius * 2) / 2,
-                            left: kCircleMargin + _itemPosByIndex(i),
-                            child: BottomBarInActiveItem(i,
-                                itemWidget:
-                                    widget.bottomBarItems[i].inActiveItem,
-                                label: widget.bottomBarItems[i].itemLabel,
-                                onTap: widget.onTap,
-                                showLabel: widget.showLabel,
-                                labelStyle: widget.itemLabelStyle),
-                          ),
-                      ],
-                    ],
+                          for (var i = 0;
+                              i < widget.bottomBarItems.length;
+                              i++) ...[
+                            if (i == currentIndex)
+                              Positioned(
+                                top: kTopMargin,
+                                left: kCircleRadius -
+                                    kCircleMargin / 2 +
+                                    _itemPosByScrollPosition(scrollPosition),
+                                child: BottomBarActiveItem(
+                                  i,
+                                  itemWidget:
+                                      widget.bottomBarItems[i].activeItem,
+                                  scrollPosition: scrollPosition,
+                                  onTap: widget.onTap,
+                                ),
+                              ),
+                            if (i != currentIndex)
+                              Positioned(
+                                top:
+                                    kMargin + (kHeight - kCircleRadius * 2) / 2,
+                                left: kCircleMargin + _itemPosByIndex(i),
+                                child: BottomBarInActiveItem(i,
+                                    itemWidget:
+                                        widget.bottomBarItems[i].inActiveItem,
+                                    label: widget.bottomBarItems[i].itemLabel,
+                                    onTap: widget.onTap,
+                                    showLabel: widget.showLabel,
+                                    labelStyle: widget.itemLabelStyle),
+                              ),
+                          ],
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           );
   }
 
   double _firstItemPosition() {
-    return (_screenWidth - kMargin * 2) * 0.1;
+    return (_screenWidth - kMargin * 2) * 0.2;
   }
 
   double _lastItemPosition() {
     return _screenWidth -
-        (_screenWidth - kMargin * 2) * 0.1 -
+        (_screenWidth - kMargin * 2) * 0.2 -
         (kCircleRadius + kCircleMargin) * 2;
   }
 
